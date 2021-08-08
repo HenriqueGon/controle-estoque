8af462945a4347295fd5bbffdbab38ec51c002d5
Henrique Gonçalves de Oliveira <henriquegon.o@hotmail.com>
2021-08-08 00:27:48 -0300

    Controller da classe empresa
    Adicionado rotas especificas para busca, inserção e exclusão das empresas

--- api-server/src/main/java/com/br/api/company/controller/CompanyController.java
+++ api-server/src/main/java/com/br/api/company/controller/CompanyController.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,20 +15,24 @@ import com.br.api.company.model.Company;
 import com.br.api.company.repository.CompanyRepository;
 
 @RestController
-@RequestMapping("/empresas")
 public class CompanyController {
 	
 	@Autowired
 	private CompanyRepository companyRepository;
 	
-	@GetMapping
-	public List<Company> listar() {
+	@GetMapping("/empresas")
+	public List<Company> findAll() {
 		return this.companyRepository.findAll();
 	}
 	
-	@PostMapping
+	@PostMapping("/empresas/incluir")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Company incluir(@RequestBody Company Company) {
-		return this.companyRepository.save(Company);
+	public Company add(@RequestBody Company company) {
+		return this.companyRepository.save(company);
+	}
+	
+	@DeleteMapping("/empresas")
+	public void remove(@RequestBody Company company) {
+		this.companyRepository.delete(company);
 	}
 }
